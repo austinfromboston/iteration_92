949ab0c94c1ecc284c8a9561f7ecd0166cc9ceba
Chris <chris@chris-1000HE.(none)>
2011-09-11 11:05:14 -0700

    interacitve structure

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
 $(function() {
-  $("#progress").accordion();
+  var views = {
+    "assessment": "#assessmentK",
+    "framework": "#frameworkK",
+    "design": "#designK"
+  },
+  changeView = function(oldID, newID) {
+    var fadeList = views[oldID].split(","),
+    showList = views[newID].split(","),
+    result = "";
+    $(fadeList).each(function(index, element) {
+      //result += "hide "+index+" "+element+"\n";
+      $(element).hide();
+    });
+    $(showList).each(function(index, element) {
+      //result += "show "+index+" "+element+"\n"; 
+      $(element).show();
+    });
+    //alert(result);
+  }
+  changeView("assessment", "assessment");
+  // top level steps
+  $("#progress").accordion({
+   change: function(event, ui) {
+     changeView(ui.oldHeader.attr('id'), ui.newHeader.attr('id'));
+     // ui.newHeader // jQuery object, activated header
+     // ui.oldHeader // jQuery object, previous header
+     // ui.newContent // jQuery object, activated content
+     // ui.oldContent // jQuery object, previous content
+   }
+  });
+  
 });
 
